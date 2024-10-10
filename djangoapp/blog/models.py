9e533250788605d3385f5befd4e649363b0f8117
Rafaelkrc <rafaelkrc@hotmail.com>
2024-10-10 19:08:48 +0100

    created resize images in blog app
    close #13

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from utils.images import resize_image
 from utils.rands import slugify_new
 from django.contrib.auth.models import User
 
@@ -99,7 +100,18 @@ class Post(models.Model):
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.title)
-        return super().save(*args, **kwargs)
+
+        current_cover_name = str(self.cover.name)
+        super_save = super().save(*args, **kwargs)
+
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900, True, 70)
+        return super_save
 
     def __str__(self) -> str:
         return self.title
