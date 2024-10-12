b2ecae864898ef0513128225df72c3b35afc00ae
Rafaelkrc <rafaelkrc@hotmail.com>
2024-10-12 18:40:25 +0100

    created search
    close #23

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,12 +1,13 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from blog.models import Post
+from django.db.models import Q
 
 PER_PAGE = 9
 
 
 def index(request):
-    posts = Post.objects.get_published()
+    posts = Post.objects.get_published()  # pyright: ignore
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -20,7 +21,7 @@ def index(request):
 
 
 def created_by(request, author_pk):
-    posts = Post.objects.get_published().filter(created_by__pk=author_pk)
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk)  # pyright: ignore
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -34,7 +35,7 @@ def created_by(request, author_pk):
 
 
 def category(request, slug):
-    posts = Post.objects.get_published().filter(category__slug=slug)
+    posts = Post.objects.get_published().filter(category__slug=slug)  # pyright: ignore
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -48,7 +49,7 @@ def category(request, slug):
 
 
 def tag(request, slug):
-    posts = Post.objects.get_published().filter(tags__slug=slug)
+    posts = Post.objects.get_published().filter(tags__slug=slug)  # pyright: ignore
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
@@ -61,6 +62,23 @@ def tag(request, slug):
     )
 
 
+def search(request):
+    search_value = request.GET.get('search', '').strip()
+    posts = Post.objects.get_published().filter(  # pyright: ignore
+        Q(title__icontains=search_value) |
+        Q(excerpt__icontains=search_value) |
+        Q(content__icontains=search_value)
+    )[:PER_PAGE]
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
+        }
+    )
+
+
 def page(request, slug):
     return render(
         request,
@@ -69,7 +87,7 @@ def page(request, slug):
 
 
 def post(request, slug):
-    post = Post.objects.get_published().filter(slug=slug).first()
+    post = Post.objects.get_published().filter(slug=slug).first()  # pyright: ignore
     return render(
         request,
         'blog/pages/post.html', {'post': post, }
